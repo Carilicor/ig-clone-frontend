26b32b083c81e632dbc2f3f10e9df70fcef0fc8f
Cari Licor <Carilicor@gmail.com>
2022-11-16 15:19:48 -0500

    add modal

---
+++ src/components/Upload.jsx
@@ -0,0 +1,9 @@
+import { Modal } from "antd"
+
+export default function Upload() {
+    return(
+        <Modal title="Upload Photo" Open={true}>
+        <p>Upload form gotes here...</p>
+        </Modal>
+    )
+}
