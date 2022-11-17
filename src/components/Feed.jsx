6ff29c9b07a76de4537c4b5340d3ef15f7b2f5e4
Cari Licor <Carilicor@gmail.com>
2022-11-17 13:42:35 -0500

    test trial local host

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react'
 import { Button,  } from 'antd'
-import Upload from "./Upload"
+import UploadModal from "./UploadModal"
 
 export default function Feed() {
     const [photoList, setPhotoList] = useState()
     const [showUpload, setShowUpload] = useState(false)
     useEffect(() => {
         fetch('https://express-ts-cgl.web.app/photos')
+        // fetch('http://localhost:5002/photos')
         .then(results => results.json())
         .then(data => setPhotoList(data))
         .catch(alert)
@@ -16,8 +17,10 @@ export default function Feed() {
             {!photoList 
             ? <p>Loading...</p>
             : <p>{photoList.length}</p> }
-            {showUpload ? <Upload/> : null}
-            <Button onClick={() => setShowUpload(true)}classname='fab' type="primary" shape="circle" size='large'>+</Button>
+            {showUpload ? <UploadModal setPhotoList={setPhotoList} setShowUpload={setShowUpload}/> : null}
+            <Button onClick={() => setShowUpload(true)}
+            className='fab' type="primary" shape="circle" 
+            size='large'>+</Button>
         </section>
        
     )
